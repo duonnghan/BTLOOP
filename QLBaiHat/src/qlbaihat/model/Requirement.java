a53d36946c21a71666cffe65677ad2ddf6f12039
xhuiklm <dsh2t97@gmail.com>
2017-11-07 03:09:43 +0700

    Merge with DangT branch and change some necessary variable name.

---
+++ QLBaiHat/src/qlbaihat/model/Requirement.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qlbaihat.model;
+
+/**
+ *
+ * @author TRAN HAI DANG
+ */
+public class Requirement {
+    String sender,receipient,phone,address,sentdate,message,status,playdate;
+    long id,songid;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceipient() {
+        return receipient;
+    }
+
+    public void setReceipient(String receipient) {
+        this.receipient = receipient;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getSentdate() {
+        return sentdate;
+    }
+
+    public void setSentdate(String sentdate) {
+        this.sentdate = sentdate;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getPlaydate() {
+        return playdate;
+    }
+
+    public void setPlaydate(String playdate) {
+        this.playdate = playdate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getSongid() {
+        return songid;
+    }
+
+    public void setSongid(long songid) {
+        this.songid = songid;
+    }
+
+    public Requirement() {
+    }
+
+    public Requirement(String sender, String receipient, String phone, String address, String sentdate, String message, String status, String playdate, long id, long songid) {
+        this.sender = sender;
+        this.receipient = receipient;
+        this.phone = phone;
+        this.address = address;
+        this.sentdate = sentdate;
+        this.message = message;
+        this.status = status;
+        this.playdate = playdate;
+        this.id = id;
+        this.songid = songid;
+    }
+}
