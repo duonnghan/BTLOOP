476cc02db53fc6d53c4c91d596d6777e3011222a
huy <lupacexi@gmail.com>
2017-11-11 16:47:01 +0700

    Dùng Schedule trong Controller

--- QLBaiHat/src/qlbaihat/model/ScheduleTableModel.java
+++ QLBaiHat/src/qlbaihat/model/ScheduleTableModel.java
@@ -8,37 +8,39 @@ package qlbaihat.model;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 import qlbaihat.controller.DataBase;
 
 public class ScheduleTableModel extends AbstractTableModel {
 
-    public Object[][] values = new Object[100][5];
+    public ArrayList<Schedule> values = new ArrayList<>();
     public final static String[] columnNames = {
-        "STT", "ID", "Tên bài hát", "Ca sĩ", "Ghi chú",""
+        "STT", "ID", "Tên bài hát", "Ca sĩ", "Ghi chú", ""
     };
 
     String sql;
     int count = 0;
-    
-    public void setSQL(String sql){
+
+    public void setSQL(String sql) {
         this.sql = sql;
     }
-    
+
     public void loaData() {
         Connection conn = DataBase.getConnection();
-        System.out.println("Opened database connection");        
+        System.out.println("Opened database connection");
         try {
             Statement state = conn.createStatement();
             ResultSet rs = state.executeQuery(sql);
             System.out.println(" - Excuted: " + sql);
             int i = 0;
             while (rs.next()) {
-                values[i][0] = i + 1;
-                values[i][1] = rs.getLong(1);
-                values[i][2] = rs.getString(2);
-                values[i][3] = rs.getString(3);
-                values[i][4] = rs.getString(4);
+                Schedule schedule = new Schedule();
+                schedule.setId(rs.getLong(1));
+                schedule.setNameSong(rs.getString(2));
+                schedule.setNameArtist(rs.getString(3));
+                schedule.setNote(rs.getString(4));
+                values.add(schedule);
                 i++;
             }
             count = i;
@@ -47,24 +49,24 @@ public class ScheduleTableModel extends AbstractTableModel {
             conn.close();
             System.out.println("Closed database connection");
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
         this.fireTableDataChanged();
     }
-    
+
     public void updateData() {
         Connection conn = DataBase.getConnection();
-        System.out.println("Opened database connection");        
+        System.out.println("Opened database connection");
         try {
             Statement state = conn.createStatement();
             int count = state.executeUpdate(sql);
-            System.out.println(" - Excuted: " + sql);         
+            System.out.println(" - Excuted: " + sql);
             System.out.println(" - Updated " + count + " rows");
             state.close();
             conn.close();
             System.out.println("Closed database connection");
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
         this.fireTableDataChanged();
     }
@@ -81,7 +83,19 @@ public class ScheduleTableModel extends AbstractTableModel {
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return values[rowIndex][columnIndex];
+        switch (columnIndex) {
+            case 0:
+                return rowIndex + 1;
+            case 1:
+                return values.get(rowIndex).getId();
+            case 2:
+                return values.get(rowIndex).getNameSong();
+            case 3:
+                return values.get(rowIndex).getNameArtist();
+            case 4:
+                return values.get(rowIndex).getNote();
+        }
+        return 0;
     }
 
     @Override
